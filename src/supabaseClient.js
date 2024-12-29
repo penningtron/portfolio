677897b7a9841d1317421f2f561283de24267a4a
nathan pennington <nathantp@gmail.com>
2024-12-29 16:26:52 -0600

    testing

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,14 +1,14 @@
 import { createClient } from '@supabase/supabase-js';
-
+import process from 'process';
 
 const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
 const supabaseAnonKey = process.env.REACT_APP_SUPABASE_ANON_KEY;
-
 console.log("Supabase URL:", process.env.REACT_APP_SUPABASE_URL);
 console.log("Supabase Anon Key:", process.env.REACT_APP_SUPABASE_ANON_KEY);
 
 if (!supabaseUrl || !supabaseAnonKey) {
-  throw new Error('Supabase URL and Anon Key are required!');
+    throw new Error('Supabase URL and Anon Key are required!');
+  
 }
 
 
