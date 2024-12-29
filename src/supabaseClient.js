6147994a88d25c66c246cd69eac015087e467233
nathan pennington <nathantp@gmail.com>
2024-12-29 16:02:05 -0600

    changes to supabase client

---
+++ src/supabaseClient.js
@@ -0,0 +1,13 @@
+import { createClient } from '@supabase/supabase-js';
+import process from 'process';
+
+const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
+const supabaseAnonKey = process.env.REACT_APP_SUPABASE_ANON_KEY;
+
+if (!supabaseUrl || !supabaseAnonKey) {
+  throw new Error('Supabase URL and Anon Key are required!');
+}
+
+const supabase = createClient(supabaseUrl, supabaseAnonKey);
+
+export default supabase;
