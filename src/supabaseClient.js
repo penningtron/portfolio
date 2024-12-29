a4cae82daf7b90f50da19eb092e33131428f30fd
nathan pennington <nathantp@gmail.com>
2024-12-29 16:59:15 -0600

    checking env vars

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -6,8 +6,7 @@ const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 
 
-// console.log("Supabase URL:", supabaseUrl);
-// console.log("Supabase Anon Key:", supabaseAnonKey);
+
 
 if (!supabaseUrl || !supabaseAnonKey) {
     throw new Error('Vite Supabase URL and Anon Key are required!');
