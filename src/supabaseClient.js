4b566467a58c66c6e507008e8b8754bfaead5905
nathan pennington <nathantp@gmail.com>
2024-12-29 16:38:41 -0600

    changed to vite

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,21 +1,16 @@
-/* eslint-disable no-undef */
-
 import { createClient } from '@supabase/supabase-js';
 
+// Access environment variables with the VITE_ prefix
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
-const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
-const supabaseAnonKey = process.env.REACT_APP_SUPABASE_ANON_KEY;
-console.log("Supabase URL:", process.env.REACT_APP_SUPABASE_URL);
-console.log("Supabase Anon Key:", process.env.REACT_APP_SUPABASE_ANON_KEY);
+// console.log("Supabase URL:", supabaseUrl);
+// console.log("Supabase Anon Key:", supabaseAnonKey);
 
 if (!supabaseUrl || !supabaseAnonKey) {
     throw new Error('Supabase URL and Anon Key are required!');
-  
 }
 
-
 const supabase = createClient(supabaseUrl, supabaseAnonKey);
-
-
 
 export default supabase;
